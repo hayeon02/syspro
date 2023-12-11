11d1f9d5f110a492d6f20ab3e4aa81464a8a9df8
hayeon02 <jhy414800@cu.ac.kr>
2023-12-11 19:50:44 +0900

    .

---
+++ chap9/prob2/waitpid.c
@@ -0,0 +1,15 @@
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int pid1, pid2, child, status;
+	printf("[%d] 부모 프로세스 시작 \n", getpid( ));
+	pid1 = fork();
+	if (pid1 == 0) {
+		printf("[%d] 자식 프로세스[1] 시작 \n", getpid( ));
+		sleep(1);
+		printf("[%d] 자식 프로세스[1] 종료 \n", getpid( ));
+		exit(1);
+	}
+}
